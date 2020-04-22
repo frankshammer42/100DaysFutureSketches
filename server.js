1086a659b376a6f81181f7f4b66b2b50e6f1fb3f
frankshammer42 <ra2901@nyu.edu>
2020-04-22 13:21:36 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const  express = require('express');
+const path = require('path');
+let sensorDataCollection;
+let app = express();
+app.use(express.static(__dirname + '/public')); app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.set('trust proxy', true);
+
+let port = process.env.port || 8080;
+
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
+
+let server = app.listen(process.env.PORT || 3000, function() {
+	console.log('Popeye server is running on', server.address().port);
+});
+
