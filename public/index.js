62939183efabe26db9fc746e9a1e01970848a5bb
frankshammer42 <ra2901@nyu.edu>
2020-04-22 23:18:28 -0400

    with button

---
+++ public/index.js
@@ -0,0 +1,39 @@
+window.onload = getAllCurrentFolders;
+
+function compare(a, b){
+    a = a.slice(3, a.length);
+    b = b.slice(3, b.length);
+    a = parseInt(a);
+    b = parseInt(b);
+    console.log(a);
+    console.log(b);
+    if (a > b){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+
+}
+
+function getAllCurrentFolders(){
+    axios.get('/Days').then(
+        function (response) {
+            let filenames = response.data;
+            filenames.sort(compare);
+            for (let i=0; i<filenames.length; i++) {
+                let buttonContainer = document.createElement('div');
+                buttonContainer.classList.add("DayButtonContainer");
+                let button = document.createElement('button');
+                button.classList.add("DayButton");
+                button.innerText = filenames[i];
+                buttonContainer.appendChild(button);
+                document.body.appendChild(buttonContainer);
+                let path = "/days/" + filenames[i] + "/home.html";
+                button.onclick = () => {
+                    window.open(path);
+                };
+            }
+        }
+    );
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const  express = require('express');
 const path = require('path');
+const fs = require('fs');
 let sensorDataCollection;
 let app = express();
 app.use(express.static(__dirname + '/public')); app.use(express.json());
@@ -10,8 +11,19 @@ app.set('trust proxy', true);
 
 let port = process.env.port || 8080;
 
+function getDirectories(path) {
+	return fs.readdirSync(path).filter(function (file) {
+		return fs.statSync(path+'/'+file).isDirectory();
+	});
+}
+
 app.get('/', function(req, res) {
 	res.render('index.html');
+});
+
+app.get('/Days', function (req, res) {
+    let result = getDirectories("./public/days");
+    res.send(result);
 });
 
 let server = app.listen(process.env.PORT || 3000, function() {
